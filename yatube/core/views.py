c844da24d65eab38ea0b773fd4e199096d12492b
Max Milenin <mvmilenin@gmail.com>
2022-10-02 13:51:41 +0300

    for review 1.1

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def page_not_found(request, exception):
-    # Переменная exception содержит отладочную информацию, 
+    # Переменная exception содержит отладочную информацию,
     # выводить её в шаблон пользователской страницы 404 мы не станем
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
